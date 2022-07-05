8e6226fd35ef26a6b2de53c1663aa27f04702e48
hasan75 <hasanjab14@gmail.com>
2022-07-05 18:51:21 +0600

    order added

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -109,6 +109,51 @@ const Dashboard = () => {
             <small className='text-muted'>Last 24 hours</small>
           </div>
         </div>
+        <div className='recent-orders'>
+          <h2>Recent Orders</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Product Name</th>
+                <th>Product Number</th>
+                <th>Payment</th>
+                <th>Status</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Najim Phone</td>
+                <td>25689</td>
+                <td>Due</td>
+                <td className='warning'>Pending</td>
+                <td className='primary'>Details</td>
+              </tr>
+              <tr>
+                <td>Najim Phone</td>
+                <td>25689</td>
+                <td>Due</td>
+                <td className='warning'>Pending</td>
+                <td className='primary'>Details</td>
+              </tr>
+              <tr>
+                <td>Najim Phone</td>
+                <td>25689</td>
+                <td>Due</td>
+                <td className='warning'>Pending</td>
+                <td className='primary'>Details</td>
+              </tr>
+              <tr>
+                <td>Najim Phone</td>
+                <td>25689</td>
+                <td>Due</td>
+                <td className='warning'>Pending</td>
+                <td className='primary'>Details</td>
+              </tr>
+            </tbody>
+          </table>
+          <a href='#'>Show All</a>
+        </div>
       </main>
     </div>
   );
