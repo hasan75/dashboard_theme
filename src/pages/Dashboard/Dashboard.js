64f39f9b23cc2824463ebf616342fddd8b6e0da3
hasan75 <hasanjab14@gmail.com>
2022-07-05 18:37:28 +0600

    added the media query for responsiveness

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -37,7 +37,7 @@ const Dashboard = () => {
           <a href='#'>
             <span class='material-icons-sharp'>question_answer</span>
             <h3>Messages</h3>
-            <span className='message-Count'>26</span>
+            <span className='message-count'>26</span>
           </a>
           <a href='#'>
             <span class='material-icons-sharp'>logout</span>
@@ -45,6 +45,71 @@ const Dashboard = () => {
           </a>
         </div>
       </aside>
+      <main>
+        <h1>Dashboard</h1>
+        <div className='date'>
+          <input type='date' />
+        </div>
+        <div className='insights'>
+          {/* sales  */}
+          <div className='sales'>
+            <span className='material-icons-sharp'>analytics</span>
+            <div className='middle'>
+              <div className='left'>
+                <h3>Total Sales</h3>
+                <h1>$21,000</h1>
+              </div>
+              <div className='progress'>
+                <svg>
+                  <circle cx='38' cy='38' r='36'></circle>
+                </svg>
+                <div className='number'>
+                  <p>81%</p>
+                </div>
+              </div>
+            </div>
+            <small className='text-muted'>Last 24 hours</small>
+          </div>
+          {/* expenses */}
+          <div className='expenses'>
+            <span className='material-icons-sharp'>bar_chart</span>
+            <div className='middle'>
+              <div className='left'>
+                <h3>Total Expenses</h3>
+                <h1>$21,000</h1>
+              </div>
+              <div className='progress'>
+                <svg>
+                  <circle cx='38' cy='38' r='36'></circle>
+                </svg>
+                <div className='number'>
+                  <p>81%</p>
+                </div>
+              </div>
+            </div>
+            <small className='text-muted'>Last 24 hours</small>
+          </div>
+          {/* income  */}
+          <div className='income'>
+            <span className='material-icons-sharp'>stacked_line_chart</span>
+            <div className='middle'>
+              <div className='left'>
+                <h3>Total Income</h3>
+                <h1>$21,000</h1>
+              </div>
+              <div className='progress'>
+                <svg>
+                  <circle cx='38' cy='38' r='36'></circle>
+                </svg>
+                <div className='number'>
+                  <p>44%</p>
+                </div>
+              </div>
+            </div>
+            <small className='text-muted'>Last 24 hours</small>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
